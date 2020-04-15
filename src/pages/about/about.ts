0e6feffe9e6460f707dd237a283dfdb745edd150
yaping.pan <pan.yaping@le-yao.com>
2020-04-15 15:16:33 +0800

    Add ScrollMenu

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,68 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CategoryData } from './mock';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+	@ViewChild('rightScroll', { read: ElementRef }) rightScroll: ElementRef;
+	private dataList = CategoryData;
+	private currentCategoryIndex = 0;
 
   constructor(public navCtrl: NavController) {
+  }
 
+  ionViewDidLoad(){
+  	let scrollEle = this.rightScroll.nativeElement.firstElementChild;
+  	console.log(scrollEle);
+  	let that = this;
+  	scrollEle.addEventListener('scroll', (ev) => {
+  		that.bindscroll(ev, that);
+  	}, false)
+  }
+
+  // 监听滑动，控制左侧栏选中状态
+  bindscroll(event:any, that) {
+    let containViewBottom;
+    let categoryItem;
+    let categoryLength = that.dataList.length;
+    if (categoryLength === 0 || categoryLength === 1) {
+      return;
+    }
+    async function f(that) {
+      
+      containViewBottom = that.rightScroll.nativeElement.getBoundingClientRect().bottom;
+      let item = { index: undefined, isbreak: false };
+        for (let i = 0; i < categoryLength; i++) {
+
+        	let cateBot = document.getElementById('category_' + i).getBoundingClientRect()
+        	if (!item.isbreak && cateBot.top < 50) {
+              item = { index: i, isbreak: false };
+            }
+            if (cateBot.top < containViewBottom && cateBot.top > 50) {
+              item = { index: i, isbreak: true };
+            }
+            if (item.isbreak || i === categoryLength - 1) {
+              categoryItem = item;
+              break;
+            }
+        }
+      let index = categoryItem.index;
+      if (index !== undefined && index !== that.currentCategoryIndex) {
+        that.currentCategoryIndex = index
+      }
+    }
+    f(this);
+  }
+
+  //点击分类项
+  scrollToIndex(index) {
+  	this.currentCategoryIndex = index;
+  	document.getElementById('category_' + index).scrollIntoView();
+  	//this.rightScroll.nativeElement.scrollIntoView('#category_' + index);
   }
 
 }
---
+++ src/pages/about/mock.ts
@@ -0,0 +1,42 @@
+export const CategoryData = [
+{
+	id: 1,
+	name: '水果',
+	image: '',
+	list: [
+		{name: '苹果', price: 5},
+		{name: '梨', price: 3},
+		{name: '香蕉', price: 6},
+		{name: '西瓜', price: 4}]
+},{
+	id: 2,
+	name: '蔬菜',
+	image: '',
+	list: [
+		{name: '青椒', price: 2.2},
+		{name: '大蒜', price: 3},
+		{name: '西红柿', price: 4},
+		{name: '芹菜', price: 2.3},
+		{name: '大白菜', price: 1}]
+},{
+	id: 3,
+	name: '零食',
+	image: '',
+	list: [
+		{name: '恰恰瓜子', price: 5},
+		{name: '德芙巧克力', price: 10},
+		{name: '康师傅红烧牛肉面', price: 4},
+		{name: '乐事原味薯片', price: 5.8},
+		{name: '咪咪虾条', price: 1},
+		{name: '可乐', price: 2.5}]
+},{
+	id: 4,
+	name: '火锅',
+	image: '',
+	list: [
+		{name: '海底捞火锅底料', price: 15},
+		{name: '肥牛片200g', price: 30},
+		{name: '安井牛肉丸100', price: 18},
+		{name: '川崎海鲜酱', price: 6.3},
+		{name: '毛肚500', price: 70}]
+}];
